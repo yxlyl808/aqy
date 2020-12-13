72e3e740b31c39e569d9736689571409f3b5a82f
yxlyl808 <74393077+yxlyl808@users.noreply.github.com>
2020-12-13 15:08:50 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,67 @@
+const exec = require('child_process').execSync;
+const fs = require('fs');
+const rp = require('request-promise');
+const download = require('download');
+
+// 公共变量
+const KEY = process.env.cookie;
+const serverJ = process.env.PUSH_KEY;
+
+
+async function downFile () {
+    const url = 'https://raw.githubusercontent.com/NobyDa/Script/master/iQIYI-DailyBonus/iQIYI.js';
+    await download(url, './');
+}
+
+async function changeFile () {
+   let content = await fs.readFileSync('./iQIYI.js', 'utf8')
+   content = content.replace(/var cookie = ''/, `var cookie = '${KEY}'`);
+   await fs.writeFileSync( './iQIYI.js', content, 'utf8')
+}
+
+async function sendNotify (text,desp) {
+  const options ={
+    uri:  `https://sc.ftqq.com/${serverJ}.send`,
+    form: { text, desp },
+    json: true,
+    method: 'POST'
+  }
+  await rp.post(options).then(res=>{
+    console.log(res)
+  }).catch((err)=>{
+    console.log(err)
+  })
+}
+
+async function start() {
+  if (!KEY) {
+    console.log('请填写 key 后在继续')
+    return
+  }
+  // 下载最新代码
+  await downFile();
+  console.log('下载代码完毕')
+  // 替换变量
+  await changeFile();
+  console.log('替换变量完毕')
+  // 执行
+  await exec("node iQIYI.js >> result.txt");
+  console.log('执行完毕')
+
+  if (serverJ) {
+    const path = "./result.txt";
+    let content = "";
+    if (fs.existsSync(path)) {
+      content = fs.readFileSync(path, "utf8");
+    }
+    let t = content.match(/【签到概览】:((.|\n)*)【签到总计】/)
+    let res = t ? t[1].replace(/\n/,'') : '失败'
+    let t2 = content.match(/【签到总计】:((.|\n)*)【账号总计】/)
+    let res2 = t2 ? t2[1].replace(/\n/,'') : '总计0'
+
+    
+    await sendNotify("" + ` ${res2} ` + ` ${res} ` + new Date().toLocaleDateString(), content);
+  }
+}
+
+start()
